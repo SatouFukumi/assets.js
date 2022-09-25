cc3045df79c19c10fde346ca9a2ad2470564f7e1
Fukumi <satoufukumii@gmail.com>
2022-09-25 19:47:31 +0700

    💀

--- ts/cursor.ts
+++ ts/cursor.ts
@@ -1,23 +1,24 @@
 export const cursor = {
-    positionX: window.innerWidth / 2,
-    positionY: window.innerHeight / 2,
+    positionX: 0,
+    positionY: 0,
     deltaX: 0,
     deltaY: 0,
     isWatching: false,
     alwaysOn: false,
 
     watch(alwaysOn: boolean = false): void {
-        if (this.isWatching) return
-        this.isWatching = true
+        if (this.alwaysOn || this.isWatching || typeof window === "undefined")
+            return
 
+        this.isWatching = true
         this.alwaysOn = alwaysOn
 
         window.addEventListener("mousemove", this.__update)
     },
 
     stop(): void {
-        if (this.alwaysOn) return
-        if (!this.isWatching) return
+        if (this.alwaysOn || !this.isWatching || typeof window === "undefined")
+            return
 
         window.removeEventListener("mousemove", this.__update)
     },
